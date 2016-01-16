edd85a6a3d8945537349233960db53a4ce697cbe
Benjamin Lo <benjamin.lo246@gmail.com>
2016-01-16 11:23:36 -0500

    initial commit

---
+++ Assets/GameSetup.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+//Reference the camera
+var mainCam : Camera;
+
+//Reference the colliders we are going to adjust
+var topWall : BoxCollider2D;
+var bottomWall : BoxCollider2D;
+var leftWall : BoxCollider2D;
+var rightWall : BoxCollider2D;
+
+function Start () { //Only set this to Update if you know the screen size can change during a playsession.
+	Screen.SetResolution (1280, 768, true);
+	//Move each wall to its edge location:
+	topWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
+	topWall.offset = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3 ( 0f, Screen.height, 0f)).y + 0.5f);
+	
+	bottomWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2, 0f, 0f)).x, 1f);
+	bottomWall.offset = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3( 0f, 0f, 0f)).y - 0.5f);
+	
+	leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);;
+	leftWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+	
+	rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);
+	rightWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float speed = 1.5f;
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+	public KeyCode moveLeft;
+	public KeyCode moveRight;
+
+	// Use this for initialization
+	void Start () { 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(moveUp))
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		if (Input.GetKey(moveDown))
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		if (Input.GetKey(moveLeft))
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		if (Input.GetKey(moveRight))
+			transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+}
