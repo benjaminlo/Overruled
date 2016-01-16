fc99d2c57866502aa705409ee30dc1d590e72430
Benjamin Lo <benjamin.lo246@gmail.com>
2016-01-16 14:16:35 -0500

    added new sprite, moved sprite animation to controller script

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,29 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class playerController : MonoBehaviour {
 
-	public float speed = 1.5f;
 	public KeyCode moveUp;
 	public KeyCode moveDown;
 	public KeyCode moveLeft;
 	public KeyCode moveRight;
 
+	public Sprite[] spritesF;
+	public Sprite[] spritesB;
+	public Sprite[] spritesR;
+	public Sprite[] spritesL;
+
+	public float framesPerSecond;
+	public float speed;
+	private SpriteRenderer spriteRenderer;
+
+	public bool endGame = false;
+
 	// Use this for initialization
 	void Start () { 
-		
+		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(moveUp))
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		index = index % spritesF.Length;
+
+		if (Input.GetKey (moveUp)) {
 			transform.position += Vector3.up * speed * Time.deltaTime;
-		if (Input.GetKey(moveDown))
+			spriteRenderer.sprite = spritesB [index];
+		}
+		if (Input.GetKey (moveDown)) {
 			transform.position += Vector3.down * speed * Time.deltaTime;
-		if (Input.GetKey(moveLeft))
+			spriteRenderer.sprite = spritesF [index];
+		}
+		if (Input.GetKey (moveLeft)) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
-		if (Input.GetKey(moveRight))
+			spriteRenderer.sprite = spritesL [index];
+		}
+		if (Input.GetKey (moveRight)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
+			spriteRenderer.sprite = spritesR [index];
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collisionInfo)
@@ -31,5 +53,10 @@ public class playerController : MonoBehaviour {
 		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
 		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
 		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+		if(collisionInfo.collider.name == "coinBlock") {
+			endGame = true;
+			print ("ENDING GAME!");
+			//SceneManager.LoadScene("Assets/scene"); //untested
+		}
 	}
 }
