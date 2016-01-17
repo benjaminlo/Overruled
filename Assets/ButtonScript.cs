38d3e3fcdd8160dbf9d548bbb27fc0446bfa6031
Mitchell Catoen <mitchellcatoen@gmail.com>
2016-01-17 03:41:37 -0500

    added new rules

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void startLevel(){
+		Application.LoadLevel ("scene");
+	}
+
+	public void shorRules(){
+		Application.LoadLevel ("scene");
+	}
+}
--- Assets/GameSetup.js
+++ Assets/GameSetup.js
@@ -18,9 +18,9 @@ function Start () { //Only set this to Update if you know the screen size can ch
 	bottomWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2, 0f, 0f)).x, 1f);
 	bottomWall.offset = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3( 0f, 0f, 0f)).y - 0.5f);
 	
-	leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);;
-	leftWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+	leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0.5f, Screen.height*2f, 0f)).y);;
+	leftWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.1f, 0f);
 	
-	rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 0f)).y);
+	rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0.5f, Screen.height*2f, 0f)).y);
 	rightWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
 }
