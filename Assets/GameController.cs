843798fa04e439c17e81a8fe1ae98b02d4a5f289
Mitchell Catoen <mitchellcatoen@gmail.com>
2016-02-17 23:46:47 -0500

    Modular Backgrounds

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public static int[] rules;
+	public static int p1Score;
+	public static int p2Score;
+
+	////////////////////////////////
+	//							  //
+	//							  //
+	//    Rule References Here    //
+	//							  //
+	//							  //
+	//							  //
+	////////////////////////////////
+
+
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		p1Score = 0;
+		p2Score = 0;
+	 
+		rules = new int[10];
+
+		rules [0] = 1;
 	}
 }
---
+++ Assets/backgroundController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundController : MonoBehaviour {
+	GameObject background;
+	Texture2D iceBackground;
+	GameObject controller;
+
+	int [] rules;
+
+	// Use this for initialization
+	void Start () {
+
+		iceBackground = Resources.Load ("IceBackground") as Texture2D;
+
+		switch (GameController.rules [0]) {
+		case 0:
+			break;
+		case 1:
+			GameObject.Find ("Background").GetComponent<Renderer> ().material.mainTexture = iceBackground;
+			break;
+		case 2:
+			GameObject.Find ("Background").GetComponent<Renderer> ().material.mainTexture = iceBackground;
+			break;
+		}
+	}
+}
