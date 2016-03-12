22a0b1263b65a7deed4a36d6b09fc087c09e98a8
Benjamin Lo <benjamin.lo246@gmail.com>
2016-03-11 23:25:00 -0500

    Implemented rule selection

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,21 @@ public class GameController : MonoBehaviour {
 	public static int p1Score;
 	public static int p2Score;
 
+	public Transform goal;
+
 	public static int[] rules;
-	private static int numRules;
-	private static int numChosenRules;
-	public static bool runRuleSelection;
+	public static int numRules;
+	public static int numChosenRules;
+	public static bool displayRuleSelection;
+	private static int selectionRule1;
+	private static int selectionRule2;
+	private static int selectionRule3;
+	private static int selectionChoice1;
+	private static int selectionChoice2;
+	private static int selectionChoice3;
+	private static string selection1;
+	private static string selection2;
+	private static string selection3;
 
 	public Texture btnTexture;
 	private GUIStyle buttonStyle;
@@ -29,15 +40,14 @@ public class GameController : MonoBehaviour {
 	rules [2]	Player size (active)
 	*/
 
-	// Use this for initialization
-	void Start () {
+	void Awake () {
 		p1Score = 0;
 		p2Score = 0;
 
 		numRules = 3;
 		numChosenRules = 0;
 		rules = new int[numRules];
-		runRuleSelection = false;
+		displayRuleSelection = false;
 
 		buttonStyle = new GUIStyle();
 		buttonFont = Resources.Load ("ButtonFont") as Font;
@@ -46,16 +56,38 @@ public class GameController : MonoBehaviour {
 		DontDestroyOnLoad (gameObject);
 	}
 
+	void Start() {
+		Instantiate (goal);
+	}
+
 	void OnGUI() {
-		if (runRuleSelection) {
-			if (GUI.Button (new Rect (Screen.width/16, Screen.height*19/20 - Screen.height*2/3, Screen.width/4, Screen.height*2/3), "Rule 1", buttonStyle)) {
-				Debug.Log ("1");
+		if (displayRuleSelection) {
+			if (GUI.Button (new Rect (Screen.width/16, Screen.height*19/20 - Screen.height*2/3, Screen.width/4, Screen.height*2/3), selection1, buttonStyle)) {
+				setRule (selectionRule1, selectionChoice1);
+				updatePlayersAndBackground ();
+				displayRuleSelection = false;
+				if (numChosenRules < numRules) 
+					Instantiate (goal);
+				
+				printRulesInfo ();
 			}
-			if (GUI.Button (new Rect (Screen.width*3/8, Screen.height*19/20 - Screen.height*2/3, Screen.width/4, Screen.height*2/3), "Rule 2", buttonStyle)) {
-				Debug.Log ("2");
+			if (GUI.Button (new Rect (Screen.width*3/8, Screen.height*19/20 - Screen.height*2/3, Screen.width/4, Screen.height*2/3), selection2, buttonStyle)) {
+				setRule (selectionRule2, selectionChoice2);
+				updatePlayersAndBackground ();
+				displayRuleSelection = false;
+				if (numChosenRules < numRules) 
+					Instantiate (goal);
+				
+				printRulesInfo ();
 			}
-			if (GUI.Button (new Rect (Screen.width*11/16, Screen.height*19/20 - Screen.height*2/3, Screen.width/4, Screen.height*2/3), "Rule 3", buttonStyle)) {
-				Debug.Log ("3");
+			if (GUI.Button (new Rect (Screen.width*11/16, Screen.height*19/20 - Screen.height*2/3, Screen.width/4, Screen.height*2/3), selection3, buttonStyle)) {
+				setRule (selectionRule3, selectionChoice3);
+				updatePlayersAndBackground ();
+				displayRuleSelection = false;
+				if (numChosenRules < numRules) 
+					Instantiate (goal);
+
+				printRulesInfo ();
 			}
 		}
 	}
@@ -67,7 +99,7 @@ public class GameController : MonoBehaviour {
 		buttonStyle.active.background = Resources.Load ("buttonPressedImage") as Texture2D;
 	}
 
-	public static int getRule() {
+	private static int getRule() {
 		int[] unchosenRules = new int[numRules];
 		int unchosenRulesArrayLength = 0;
 
@@ -80,18 +112,39 @@ public class GameController : MonoBehaviour {
 		return unchosenRules[Random.Range (0,unchosenRulesArrayLength)];
 	}
 
-	public static void setRule(int rule, int choice) {
+	public static void runRuleSelection() {
+		selectionRule1 = getRule ();
+		selectionRule2 = getRule ();
+		selectionRule3 = getRule ();
+		selectionChoice1 = Random.Range (1,3);
+		selectionChoice2 = Random.Range (1,3);
+		selectionChoice3 = Random.Range (1,3);
+
+		selection1 = "Rule: " + selectionRule1 + "\nChoice: " + selectionChoice1;
+		selection2 = "Rule: " + selectionRule1 + "\nChoice: " + selectionChoice2;
+		selection3 = "Rule: " + selectionRule3 + "\nChoice: " + selectionChoice3;
+
+		displayRuleSelection = true;
+	}
+
+	private static void setRule(int rule, int choice) {
 		if (numChosenRules < numRules) {
 			rules [rule] = choice;
 			numChosenRules++;
 		}
 	}
 
-	public static void getRulesInfo() {
+	public static void printRulesInfo() {
 		print (
 			"Number of chosen rules = " + numChosenRules + "\n" +
 			"Number of unchosen rules = " + (numRules - numChosenRules)
 		);
 		print ("Rules Array: " + rules [0] + rules [1] + rules [2]);
+	}
+
+	private void updatePlayersAndBackground() {
+		BackgroundController.updateBackground ();
+		GameObject.Find ("Player1").GetComponent<PlayerController>().updatePlayer ();
+		GameObject.Find ("Player2").GetComponent<PlayerController>().updatePlayer ();
 	}
 }
--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -4,19 +4,8 @@ using System.Collections;
 public class GoalController : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
-		//GameController.runRuleSelection = true;
-
-		int rule = GameController.getRule ();
-		int choice = Random.Range (1,3);
-		GameController.setRule (rule, choice);
-
-		BackgroundController.updateBackground ();
-
-		GameObject.Find ("Player1").GetComponent<PlayerController>().updatePlayer ();
-		GameObject.Find ("Player2").GetComponent<PlayerController>().updatePlayer ();
-
-		GameController.getRulesInfo ();
-
-		//Destroy (gameObject);
+		if (GameController.numChosenRules < GameController.numRules)
+			GameController.runRuleSelection();
+		Destroy (gameObject);
 	}
 }
